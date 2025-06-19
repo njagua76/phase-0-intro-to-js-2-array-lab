cb28351b2c0217766940b22f4cb0f6baf952c85d
Ann Gathoni <theanngathoni@gmail.com>
2025-06-20 00:46:36 +0300

    my changes

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+// The cats array
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive Functions
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Non-Destructive Functions
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
